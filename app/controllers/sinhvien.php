27f7d0cdbdf7acf9d0c49a4b50d3ef7b5d0f7db2
viethz21 <nquocviet1210@gmail.com>
2026-06-09 23:28:35 +0700

    feat<sinhvien>: paging

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -3,11 +3,12 @@
     require_once '../app/core/Controller.php';
     require_once '../app/models/sinhvienModel.php';
     class sinhvien extends Controller {
-        public function index(){
+        public function index($limit = 5, $offset = 0){
             $sinhvienModel = $this->model('sinhvienModel');
-            $sinhviens = $sinhvienModel->getAllSinhVien();
-
-            $this->view("layout/masterlayout", ["viewname" => "sinhvien/index","sinhviens" => $sinhviens, "title" => "Danh sach sinh vien"]);
+            $result = $sinhvienModel->paging($limit, $offset);
+            $sinhviens = $result['sinhviens'];
+            $totalpage = $result['totalpage'];
+            $this->view('layout/masterlayout', ['viewname' => 'sinhvien/index', 'sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên', 'totalpage'=>$totalpage]);
         }
         public function create(){
            require_once '../app/views/sinhvien/create.php';
--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Danh sách sinh viên</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
     table {
     border-collapse: collapse;
@@ -38,5 +39,26 @@
             </tr>
         <?php endforeach; ?>
     </table>
+    <nav aria-label="Page navigation" class="mt-4">
+    <ul class="pagination justify-content-center">
+
+        <?php
+            $pageSize = 5;
+
+            for ($i = 1; $i <= $totalpage; $i++) {
+                $offset = ($i - 1) * $pageSize;
+        ?>
+                <li class="page-item">
+                    <a class="page-link"
+                       href="/sinhvien/index/<?php echo $pageSize; ?>/<?php echo $offset; ?>">
+                        <?php echo $i; ?>
+                    </a>
+                </li>
+        <?php
+            }
+        ?>
+
+    </ul>
+</nav>
 </body>
 </html>
